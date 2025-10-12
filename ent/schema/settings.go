821f25e1d204589f9cb20d61d92bc0de5e01060b
Ilya <ilya.yunkins@gmail.com>
2025-10-12 14:55:50 +0300

    add settings schema with fields for organization and invoicing configuration

---
+++ ent/schema/settings.go
@@ -0,0 +1,22 @@
+package schema
+
+import (
+	"entgo.io/ent"
+	"entgo.io/ent/schema/field"
+)
+
+type Settings struct{ ent.Schema }
+
+func (Settings) Fields() []ent.Field {
+	return []ent.Field{
+		field.Int("singleton_id"),
+		field.String("org_name").Default(""),
+		field.String("address").Default(""),
+		field.String("invoice_prefix").Default(""),
+		field.Int("next_seq").Default(1),
+		field.Int("invoice_day_of_month").Default(1),
+		field.Bool("auto_issue").Default(false),
+		field.String("currency").Default("EUR"),
+		field.String("locale").Default("en_EN"),
+	}
+}
